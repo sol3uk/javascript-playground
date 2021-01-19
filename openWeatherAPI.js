01a6dcdfd20a1533259a72bf3f88463ed40ee03a
Ben Ashley <ben@sol3.me>
2021-01-19 20:54:02 +0000

    Commented

--- openWeatherAPI.js
+++ openWeatherAPI.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
 var domain = "https://api.openweathermap.org/data/2.5/"
 var endpoint = "weather"
+//This is just a test key, if you steal it you're mean
 var educationalKey = "4561dbb858c01c0096262a7277c1ba4a"
 var citySearch = "london"
+//Example, build url with template literals
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
 var url = `${domain+endpoint}?q=${citySearch}&appid=${educationalKey}`
 
-
+//https://api.jquery.com/jquery.ajax/
   $.ajax({
       method: "GET",
       url: url
